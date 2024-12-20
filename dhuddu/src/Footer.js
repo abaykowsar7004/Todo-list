d9b58ba134c782d34b9c4c92d52d2c7beba8fca6
abay-kowsar <abaykowsar7004@gmail.com>
2024-12-20 22:37:39 +0530

    first commit

---
+++ dhuddu/src/Footer.js
@@ -0,0 +1,9 @@
+const Footer = ({ length }) => {
+    return (
+        <footer>
+            <p>{length} List {length < 2 ? "item" : "items"}</p>
+        </footer>
+    )
+}
+
+export default Footer
